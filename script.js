5dc01119fa3baa9e18f35e6c6f0e405180507947
Manoj V <manojvenki098@gmail.com>
2023-10-13 09:16:59 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function backToHome(){
+    document.querySelector(".nav_btn").href="home.html";
+}
+
+document.querySelector("#box_1").addEventListener("click",()=>{
+    window.location.href="subpage_1.html";
+});
+document.querySelector("#box_2").addEventListener("click",()=>{
+    window.location.href="subpage_2.html";
+});
+document.querySelector("#box_3").addEventListener("click",()=>{
+    window.location.href="subpage_3.html";
+});
+document.querySelector("#box_4").addEventListener("click",()=>{
+    window.location.href="subpage_4.html";
+});
+document.querySelector("#box_5").addEventListener("click",()=>{
+    window.location.href="subpage_5.html";
+});
+document.querySelector("#box_6").addEventListener("click",()=>{
+    window.location.href="subpage_6.html";
+});
+document.querySelector("#box_7").addEventListener("click",()=>{
+    window.location.href="subpage_7.html";
+});
+document.querySelector("#box_8").addEventListener("click",()=>{
+    window.location.href="subpage_8.html";
+});
